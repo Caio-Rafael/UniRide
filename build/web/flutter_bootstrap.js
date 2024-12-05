c45f64a6da0ce7fe3bb96878cbfac30214a2b2e5
Caio-Rafael <caiorafaelsn@gmail.com>
2024-12-05 16:40:51 -0300

    commit 15 - ajuste web para deploy

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3576966998"
+    serviceWorkerVersion: "1273408265"
   }
 });
